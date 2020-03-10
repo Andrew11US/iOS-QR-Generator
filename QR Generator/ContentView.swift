853824c1c32c0fe3ff2716c91e14855cb42c862a
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-03-10 21:57:20 +0100

    Initial Commit

---
+++ QR Generator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QR Generator
+//
+//  Created by Andrew on 3/10/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
